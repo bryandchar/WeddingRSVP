4ae79db4735f0b7ea6dfe1811a9fe6487db9dc99
charitychong <70442303+charitychong@users.noreply.github.com>
2022-12-23 00:23:44 +0800

    1st upload
    - landing made

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+
+$('.food__btn').click(function(){
+
+  $(this).next( ".instructions" ).slideToggle('slow');
+});
+
+
+
+
+$('.close').click(function(){
+
+  $(this).parent().slideToggle('slow');
+});
+
+
+
+$('.blend').click(function(){
+
+  $(this).parent().slideToggle('slow');
+});
